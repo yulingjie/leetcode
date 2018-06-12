d767f47f9b93ce8d460ca0b64c3df238ab5df0e9
lingjiey@gmail.com <lingjiey@gmail.com>
2018-06-12 14:18:27 +0800

    first suffixt tree edition

--- suffixTree.c
+++ suffixTree.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include<stdlib.h>
+#include<string.h>
 #define MAX_CHAR 256
 
 
@@ -7,8 +8,8 @@
 
 typedef struct suffix_tree_node
 {
-	SuffixTreeNode* children[MAX_CHAR];
-	SuffixTreeNode* suffixLink;
+	struct suffix_tree_node* children[MAX_CHAR];
+	struct suffix_tree_node* suffixLink;
 	int start;
 	int* end;
 	int suffixIndex;
@@ -28,28 +29,36 @@ typedef struct suffix_active_point
 SuffixTreeNode* create_node(int start, int * end)
 {
 	SuffixTreeNode* node = (SuffixTreeNode*)malloc(sizeof(SuffixTreeNode));
-	node.start =start;
-	node.end = end;
-	node.suffixIndex = -1;
-	node.suffixLink = NULL;
+	node->start =start;
+	node->end = end;
+	node->suffixIndex = -1;
+	node->suffixLink = NULL;
 	for(int i = 0; i < MAX_CHAR; ++i)
 	{
-		node.children[i] = NULL;
+		node->children[i] = NULL;
 	}
-	node.isLeaf = 1;
+	node->isLeaf = 1;
+	return node;
 }
 int edge_length(SuffixTreeNode* node)
 {
 	return *(node->end) - node->start + 1;
 }
-
-void set_suffix_index_by_dfs(SuffixTreeNode* node, int labelHeight)
+void print(int i, int j, char* S)
+{
+	int k;
+	for(k = i; k <=j; ++k)
+	{
+		printf("%c", S[k]);
+	}
+}
+void set_suffix_index_by_dfs(SuffixTreeNode* node, int labelHeight,char *S,int size)
 {
 	if(node == NULL) return;
 	
 	if(node->start != -1)
 	{
-		print(node->start, *(node->end));
+		print(node->start, *(node->end), S);
 	}
 	int leaf = 1;
 	int i ;
@@ -60,15 +69,16 @@ void set_suffix_index_by_dfs(SuffixTreeNode* node, int labelHeight)
 			if(leaf == 1&& node->start != -1)
 			{
 				printf(" [%d]\n", node->suffixIndex);
-			}
+			} 
+			
 			leaf = 0;
 			set_suffix_index_by_dfs(node->children[i], 
-			labelHeight + edge_length(node->children[i]));
+			labelHeight + edge_length(node->children[i]),S, size);
 		}
 	}
 	if( leaf == 1)
 	{
-		node->suffixIndex = size- labelHeight;
+		node->suffixIndex = size - labelHeight;
 		printf(" [%d]\n", node->suffixIndex);
 	}
 }
@@ -89,19 +99,19 @@ void free_suffix_tree_by_post_order(SuffixTreeNode* node)
 	}
 	free(node);
 }
-int walk_down(ActivePoint&ap,SuffixTreeNode* node, char * S)
+int walk_down(ActivePoint* ap,SuffixTreeNode* node, char * S)
 {
-	if(ap.activeLength >= edge_length(node))
+	if(ap->activeLength >= edge_length(node))
 	{
-		ap.activeNode = node;
-		ap.activeLength -= edge_length(node);
-		ap.activeTree = S[node->end + 1];
+		ap->activeNode = node;
+		ap->activeLength -= edge_length(node);
+		ap->activeTree = S[*(node->end) + 1];
 		return 1;
 	}
 	return 0;
 }
 
-void construct_suffix_tree(char* s)
+void construct_suffix_tree(char* s, int size)
 {
 	
 	char* S = s;
@@ -113,10 +123,10 @@ void construct_suffix_tree(char* s)
 	
 	SuffixTreeNode* root = create_node(-1, rootEnd);
 	
-	ActivePoint ap;
-	ap.activeNode = root;
-	ap.activeLength = 0;
-	ap.activeTree = '\0';
+	ActivePoint* ap= (ActivePoint*) malloc(sizeof(ActivePoint));
+	ap->activeNode = root;
+	ap->activeLength = 0;
+	ap->activeTree = '\0';
 	int phase = 0;
 	int remainingSuffixCount = 0;
 	
@@ -124,44 +134,45 @@ void construct_suffix_tree(char* s)
 	
 	while(S[phase] != '\0')
 	{
+		*endPos = phase;
 		++ remainingSuffixCount;
 		while(remainingSuffixCount > 0)
 		{
-			if(ap.activeLength == 0) ap.activeTree = S[phase];
+			if(ap->activeLength == 0) ap->activeTree = S[phase];
 			
-			if(ap.activeNode->children[ap.activeTree] == NULL)
+			if(ap->activeNode->children[ap->activeTree] == NULL)
 			{
-				ap.activeNode->children[ap.activeTree] = create_node(phase,endPos);
+				ap->activeNode->children[ap->activeTree] = create_node(phase,endPos);
 				
 				if(lastNewNode != NULL)
 				{
-					lastNewNode ->suffixLink = ap.activeNode;
+					lastNewNode ->suffixLink = ap->activeNode;
 					lastNewNode = NULL;
 				}
 			}
 			else
 			{
-				SuffixTreeNode* next = ap.activeNode->children[ap.activeTree];
+				SuffixTreeNode* next = ap->activeNode->children[ap->activeTree];
 				if(walk_down(ap, next, S)) continue;
 				
-				if(S[next->start + ap.activeLength] == S[phase])
+				if(S[next->start + ap->activeLength] == S[phase])
 				{
-					if(lastNewNode != NULL && activeNode != root)
+					if(lastNewNode != NULL && ap->activeNode != root)
 					{
-						lastNewNode -> suffixLink = ap.activeNode;
+						lastNewNode -> suffixLink = ap->activeNode;
 						lastNewNode = NULL;
 					}
-					++ap.activeLength;
+					++ap->activeLength;
 					break;
 				}
 				
 				int* splitEnd = (int*)malloc(sizeof(int));
-				*splitEnd = next->start + ap.activeLength - 1;
+				*splitEnd = next->start + ap->activeLength - 1;
 				SuffixTreeNode* splitNode = create_node(next->start, splitEnd);
-				activeNode->children[activeNode->activeTree] = splitNode;
+				ap->activeNode->children[ap->activeTree] = splitNode;
 				
 				splitNode->children[S[phase]] = create_node(phase, endPos);
-				next->start += ap.activeLength;
+				next->start += ap->activeLength;
 				splitNode->children[S[next->start]] = next;
 				
 				if(lastNewNode != NULL)
@@ -172,18 +183,19 @@ void construct_suffix_tree(char* s)
 				lastNewNode = splitNode;
 			}
 			-- remainingSuffixCount;
-			if(ap.activeNode == root && ap.activeLength > 0)
+			if(ap->activeNode == root && ap->activeLength > 0)
 			{
-				-- ap.activeLength;
-				ap.activeTree = S[phase - remainingSuffixCount + 1];
-			}else if(ap.activeNode != root)
+				-- ap->activeLength;
+				ap->activeTree = S[phase - remainingSuffixCount + 1];
+			}else if(ap->activeNode != root)
 			{
-				ap.activeNode = ap.activeNode->suffixLink;
+				ap->activeNode = ap->activeNode->suffixLink;
 			}
 		}
+		++ phase;
 	}
 	int labelHeight = 0;
-	set_suffix_index_by_dfs(root, labelHeight);
+	set_suffix_index_by_dfs(root, labelHeight,s, size);
 	
 	free_suffix_tree_by_post_order(root);
 
@@ -191,6 +203,6 @@ void construct_suffix_tree(char* s)
 }
 int main(int argc, char* argv[])
 {
-	char[] text = "abcabxabcd$";
-	construct_suffix_tree(text);
+	char text[] = "abcabxabcd$";
+	construct_suffix_tree(text, (int)strlen(text));
 }
