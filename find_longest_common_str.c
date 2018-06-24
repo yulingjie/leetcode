654a692fc59b0300e2aa27a2649f01d030fddd41
unknown <yulingjie@cyou-inc.com>
2018-06-24 11:37:19 +0800

    refine

--- find_longest_common_str.c
+++ find_longest_common_str.c
@@ -103,7 +103,45 @@ void printSuffixTree(SuffixTreeNode* node, char in_str[])
 		printf(" [%d]\n", node->suffixIndex);
 	}
 }
+// X 1, Y 2, XY 3
+int doTraversal(SuffixTreeNode* node,char in_str[], int label_height, int * pmax_height, int * pstart_index, int *pcur_start_index)
+{
+    if(node == NULL) return 0;
+    if(node->suffixIndex == -1) // internal node
+    {
+        int flag = 0;
+        int xy_start_index = -1;
 
+        for(int i = 0; i < MAX_CHAR; ++i)
+        {
+            if(node->children[i] == NULL) continue;
+            
+            int child_label_height = GetEdgeLength(node->children[i]) + label_height;
+            
+            int rt =  doTraversal(node->children[i],in_str, child_label_height, pmax_height, pstart_index, pcur_start_index);        
+            xy_start_index = *pcur_start_index;
+            flag |= rt;           
+        }
+        if(flag == 3 && label_height > (*pmax_height))
+        {
+            *pmax_height = label_height;
+            *pstart_index = xy_start_index;
+        }
+
+        return flag;
+    }
+    else 
+    {
+        *pcur_start_index = node->suffixIndex;
+        for(int j = node->start; j < *(node->pend); ++j)
+        {
+            if(in_str[j] == '#') return 1;            
+        }        
+        if(in_str[*(node->pend)] == '$') return 2;
+        return node->suffixIndex;
+    }
+
+}
 void findLongestCommonStr(char in_str1[], char in_str2[])
 {
     char* in_str = (char*)malloc(strlen(in_str1) + strlen(in_str2) + 1);
@@ -112,14 +150,16 @@ void findLongestCommonStr(char in_str1[], char in_str2[])
     printf("joint string : %s\n", in_str);
 
     SuffixTreeNode* node = SuffixTreeConstruct(in_str);
-    setNodeTypeDFS(node, in_str);
-    printf("=== print me ==\n");
-    printSuffixTree(node, in_str);
-    int depth = 0;
-    
-    int start_index = 0;
-    findDeepestNode(node,in_str,0, &depth,&start_index);
-    
+   // setNodeTypeDFS(node, in_str);
+   // printf("=== print me ==\n");
+   // printSuffixTree(node, in_str);
+   // int depth = 0;    
+   // int start_index = 0;
+   // findDeepestNode(node,in_str,0, &depth,&start_index);
+   int depth = 0;
+   int start_index = 0;
+   int cur_start_index = -1;
+    doTraversal(node, in_str,0, &depth, &start_index, &cur_start_index); 
     int sz = depth;
     char* output = (char*)malloc(sizeof(char)* (sz + 1));
     strncpy(output, in_str + start_index, sz);
