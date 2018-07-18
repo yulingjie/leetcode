ecb9fd81ab83f88f22efaa028f707f8d9fca243a
unknown <yulingjie@cyou-inc.com>
2018-07-18 14:55:04 +0800

    add

--- str2int.c
+++ str2int.c
@@ -16,34 +16,74 @@ int str2int(char* in_str)
     memset(reverse_str, '\0', strlen(in_str) + 1);
     char* c_ite = in_str;
     int len = 0;
-    int sign = 1;
-    while(*c_ite != '\0')
+    int sign = 0;
+    int beg = 0;
+    for(;*c_ite != '\0'; ++c_ite)
     {        
-        if(*c_ite == ' ') continue;
-        if(*c_ite == '-') sign = -1;
-        if(!isCharNumber(*c_ite)) return 0;
+        if(*c_ite == ' ')
+        {
+            if(beg == 1)
+            {
+                break;
+            }
+            continue;
+        } 
+        if(*c_ite == '-') 
+        {
+            if(sign != 0) break;
+            sign = -1;
+            if(beg == 1)
+            {
+                break;
+            }
+            beg = 1;
+            continue;
+        }
+        if(*c_ite == '+')
+        {
+            if(sign != 0) break;
+            sign = 1;
+            if(beg == 1)
+            {
+                break;
+            }
+            beg = 1;
+            continue;
+        }
+
+        if(!isCharNumber(*c_ite)) break;
+        if(beg == 0) beg = 1;
+        if(sign == 0) sign = 1;
         reverse_str[len] = *c_ite;
-        ++len;
+        ++len;        
     }
+    if(sign == 0) sign = 1;
+    reverse_str[len] = '\0';
+    
     if(len <= 0) return 0;
     int rt = 0;
-    int delta = 10 * sign;
-    int lm = sign ? INT_MAX : INT_MIN;
+    int delta = 10;
+    int lm = (sign >0) ? INT_MAX : INT_MIN;
+    
     for(int i = 0; i < len; ++i)
     {
         int orig = rt;
         rt*= delta;
-        if(rt / delta != orig) return 0;
-        int n = c2i(reverse_str[i]);
-        if(sign * (lm - rt) < n) return 0;
-        rt += (sign * n);
+        if(rt / delta != orig) return lm;
+        int n = c2i(reverse_str[i]) * sign;
+        if(sign < 0 && ((lm - rt) > n)) return lm;
+        if(sign > 0 && ((lm - rt) < n)) return lm;
+        //if(sign * (lm - rt) < n) return lm;
+        rt += n ;
     }
     free(reverse_str);
     reverse_str = NULL;
-    return rt;
+    return rt ;
 }
 
 int main(int argc,  char* argv[])
 {
-    
+    char * str = "   0+1 42";
+    int rt = str2int(str);
+    printf("%d\n", rt);
 }
---
+++ zigzag.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char* convert(char* in_str, int numRows)
+{
+    int nlen = strlen(in_str);
+    int num_per_bach = numRows * 2 -2;
+    char* rt_str = (char*)malloc(sizeof(char)* (nlen + 1));
+    int index = 0;
+    for(int row = 0; row < numRows; ++row)
+    {
+        
+    }
+}
